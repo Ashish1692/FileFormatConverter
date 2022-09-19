c5e53984d21bfe44e0cd2f3a20bd82b3264db403
Ashish Moghe <ashishmoghe6@gmail.com>
2022-09-19 21:24:06 +0530

    new images added

--- server.mjs
+++ server.mjs
@@ -3,8 +3,10 @@ const app = express();
 import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
 import hbs from 'hbs';
-import './src/dbConn.mjs';
-import newModel from './src/dbSchema.mjs';
+
+const port = process.env.PORT || 3000
+// import './src/dbConn.mjs';
+// import newModel from './src/dbSchema.mjs';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -15,21 +17,20 @@ app.set('view engine', 'hbs');
 app.use('/', express.static(join(process.cwd(), 'public')))
 
 
-app.get('/', async (req, res) => {
-    // res.render('index')
-    try {
-        const result = await newModel.find()
-        res.render('index', { data: result });
-        console.log(result)
-    } catch (error) {
-        console.log(error);
-    }
+app.get('/', (req, res) => {
+    res.render('index')
+    // try {
+    //     const result = newModel.find()
+    //     res.render('index', { data: result });
+    // } catch (error) {
+    //     console.log(error);
+    // }
 })
 app.get('/converterDashboard', (req, res) => {
     res.render('converterDashboard')
 })
 
 
-app.listen(3000, () => {
-    console.log('http://localhost:3000');
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 })
