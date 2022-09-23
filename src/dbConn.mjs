5b4db98e0a2d227a45efb1f1b6a5a413db3fa241
Ashish Moghe <ashishmoghe6@gmail.com>
2022-09-23 19:08:25 +0530

    mongo atlas

--- src/dbConn.mjs
+++ src/dbConn.mjs
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect('mongodb://localhost:27017/ConverterDB', {
+mongoose.connect('mongodb+srv://ashmongodb:mongoash762583@cluster0.6kaenjq.mongodb.net/ConverterDB?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => { console.log("MongoDB is connected..."); })
